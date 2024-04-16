669bf30ac098a3e5e824f71d436b4887ede7f82a
itsyaboi <91355411+ayitsyaboi@users.noreply.github.com>
2024-04-16 18:20:07 -0400

    yeet

--- aiuser/functions/tool_call.py
+++ aiuser/functions/tool_call.py
@@ -1,38 +1,27 @@
 
+from dataclasses import asdict
 
-import logging
+from redbot.core import Config, commands
 
-from aiuser.functions.tool_call import ToolCall
-from aiuser.functions.types import (Function, Parameters,
-                                                  ToolCallSchema)
-
-logger = logging.getLogger("red.bz_cogs.aiuser")
+from aiuser.functions.types import ToolCallSchema
 
 
-class NoResponseToolCall(ToolCall):
-    schema = ToolCallSchema(function=Function(
-        name="do_not_respond",
-        description="Choose to not respond to the messages, by ignoring the conversation",
-        parameters=Parameters(
-            properties={
-                "reason": {
-                    "type": "string",
-                    "description": "The reason for the decision to not respond",
-                },
-                "respond": {
-                    "type": "boolean",
-                    "description": "Whether or not to not respond",
-                }
-            },
-            required=["reason", "respond"]
-        )))
-    function_name = schema.function.name
+class ToolCall:
+    schema: ToolCallSchema = None
+    function_name: str = None
 
-    async def _handle(self, arguments):
-        if arguments["respond"]:
-            return None
+    def __init__(self, config: Config, ctx: commands.Context):
+        self.config = config
+        self.ctx = ctx
+        self.bot = ctx.bot
 
-        request = arguments["request"]
-        request.completion = ""
-        logger.debug(f"Decided to not respond in guild {self.ctx.guild.id} because: \"{arguments['reason']}\"")
-        return None
+    def run(self, arguments: dict, available_tools: list):
+        self.remove_tool_from_available(available_tools)
+        return self._handle(arguments)
+
+    def _handle(arguments: dict):
+        raise NotImplementedError
+
+    def remove_tool_from_available(self, available_tools: list):
+        if self.schema in available_tools:
+            available_tools.remove(self.schema)
--- aiuser/messages_list/entry.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import Literal, Union
-
-
-@dataclass(frozen=True)
-class MessageEntry:
-    role: Literal['user', 'assistant', 'system']
-    content: Union[str, list]
